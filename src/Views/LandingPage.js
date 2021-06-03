176452bebb7bf4a39b6105ed6b1d0be3b8a03767
Meagan Ramey <meagan.ramey@yahoo.com>
2021-06-03 15:25:44 -0400

    Stubbed out mild routing. A navbar file and landingpage file.

---
+++ src/Views/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar.js'
+
+
+const LandingPage = (props) => {
+
+    return (
+        <>
+            <NavBar />
+            <h1>Some kind of bootstrap or materialize card here</h1>
+        </>
+    )
+}
+
+export default LandingPage
---
+++ src/Views/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const NavBar = (props) => {
+
+
+    return (
+        <>
+        This is the NavBar
+        </>
+    )
+}
+
+export default NavBar
